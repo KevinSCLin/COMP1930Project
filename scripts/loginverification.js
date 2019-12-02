75ec81b6d81bec171e2c0745aeccb72f41ba42a3
vkub1 <kubliyvlad@gmail.com>
2019-12-01 18:03:55 -0800

    conflict

--- scripts/loginverification.js
+++ scripts/loginverification.js
@@ -5,8 +5,4 @@ if (user){
 else {
   window.location.href="redirect.html";
 }
-document.getElementById("logoutbutton").onclick=redirect
-function redirect(){
-  localStorage.clear()
-  window.location.href="index.html"
-}
+
---
+++ scripts/logoutFunction.js
@@ -0,0 +1,4 @@
+document.getElementById("logoutbutton").onclick= function (){
+    localStorage.clear()
+    window.location.href="index.html"
+  }
