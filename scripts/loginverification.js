4a7f5f9f1a620edfe153c741c33d699ef4447d01
vkub1 <kubliyvlad@gmail.com>
2019-12-04 17:22:34 -0800

    UX updates to main and summary

--- scripts/loginverification.js
+++ scripts/loginverification.js
@@ -4,4 +4,12 @@ if (user) {
 } else {
     window.location.href = "redirect.html";
 }
+function displayHelpfulMessage(message){
+    let messageBox= document.createElement("div");
+    messageBox.id = message;
+    messageBox.className = "Goals";
+    messageBox.innerHTML = message;
+    return messageBox
+    
+}
 
